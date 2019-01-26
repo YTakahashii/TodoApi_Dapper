dc829ce63f53f212030b8051de63c3226298d0f1
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-01-26 11:37:26 +0900

    Startの設定

--- TodoAPi-Dapper/Startup.cs
+++ TodoAPi-Dapper/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TodoAPi_Dapper.Models.Persistance;
 
 namespace TodoAPi_Dapper
 {
@@ -26,6 +27,8 @@ namespace TodoAPi_Dapper
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IUnitOfWork, UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
